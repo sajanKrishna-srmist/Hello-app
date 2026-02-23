6a8e691725738e672bb33f81bae7a1051d02cc80
sajanKrishna-srmist <sk2914@srmist.edu.in>
2026-02-23 08:36:09 +0530

    Add files via upload

---
+++ UC02.java
@@ -0,0 +1,21 @@
+/**
+ * OOPSBannerApp UC2 – Banner Display Application
+ *
+ * Displays the word "OOPS" in banner format using asterisks.
+ *
+ * @author Developer
+ * @version 2
+ */
+
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+
+        System.out.println(" *****    *****    ******    ***** ");
+        System.out.println("*     *  *     *   *     *  *     *");
+        System.out.println("*     *  *     *   ******   *      ");
+        System.out.println("*     *  *     *   *        *   ***");
+        System.out.println(" *****    *****    *         ***** ");
+
+    }
+}
